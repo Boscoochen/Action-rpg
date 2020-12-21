ef445a81b1236b2199187b1de0343976fa3b06e0
Boscoochen <boscochen203@gmail.com>
2020-12-20 20:47:49 -0600

    add enemy bat death effect

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
@@ -17,3 +18,6 @@ func _on_Hurtbox_area_entered(area):
 
 func _on_Stats_no_health() -> void:
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
